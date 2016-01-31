a857917c4f6c54883c5bbc8e3fae2db9f8db02d5
tliebl <tim78317@gmail.com>
2016-01-31 11:36:34 -0600

    First Commit

---
+++ src/java/model/RectangleService.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author tliebl
+ */
+public class RectangleService {
+    
+    public double areaOfRectangle(double length, double width){
+        double area = length * width;
+        return area;
+    }
+    
+}
